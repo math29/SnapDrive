c551f3ed7e3cb0b8253930880d8a3705e519f0f1
breizhers <contact@yoann-diquelou.fr>
2015-03-31 21:25:14 +0200

    update SmsReceiver

--- app/src/main/java/com/snapdrive/snapdrive/SmsReceiver.java
+++ app/src/main/java/com/snapdrive/snapdrive/SmsReceiver.java
@@ -16,37 +16,7 @@ public class SmsReceiver extends BroadcastReceiver{
 
     public void onReceive(Context context, Intent intent) {
 
-        // Retrieves a map of extended data from the intent.
-        final Bundle bundle = intent.getExtras();
-
-        try {
-
-            if (bundle != null) {
-
-                final Object[] pdusObj = (Object[]) bundle.get("pdus");
-
-                for (int i = 0; i < pdusObj.length; i++) {
-
-                    SmsMessage currentMessage = SmsMessage.createFromPdu((byte[]) pdusObj[i]);
-                    String phoneNumber = currentMessage.getDisplayOriginatingAddress();
-
-                    String senderNum = phoneNumber;
-                    String message = currentMessage.getDisplayMessageBody();
-
-                    Log.i("SmsReceiver", "senderNum: " + senderNum + "; message: " + message);
-
-
-                    // Show Alert
-                    int duration = Toast.LENGTH_LONG;
-                    Toast.makeText(context,
-                            "senderNum: "+ senderNum + ", message: " + message, duration).show();
-
-                } // end for loop
-            } // bundle is null
-
-        } catch (Exception e) {
-            Log.e("SmsReceiver", "Exception smsReceiver" +e);
-
-        }
+        SmsApi api = new SmsApi(context);
+        Sms_s sms = api.getLastSms(intent);
     }
 }
