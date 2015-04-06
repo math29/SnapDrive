486dbde40b47c547e1c95347afbab5240d7531d1
Breizhers <contact@yoann-diquelou.fr>
2015-04-06 14:22:31 +0200

    set video path when record

--- app/src/main/java/com/snapdrive/snapdrive/AppPreferences.java
+++ app/src/main/java/com/snapdrive/snapdrive/AppPreferences.java
@@ -12,37 +12,66 @@ public class AppPreferences {
     SharedPreferences prefs;
     private Context mCtx;
 
+    /**
+     * Constructeur
+     * @param context
+     */
     public AppPreferences(Context context){
         mCtx = context;
         prefs = PreferenceManager.getDefaultSharedPreferences(mCtx);
     }
 
+    /**
+     * do the user want to have his messages read?
+     * @return: yes he want, no he doesn't
+     */
     public boolean isActivate(){
         return prefs.getBoolean("activated",false);
     }
 
+    /**
+     * the user choose wether he want to activate or not
+     * @param activation
+     */
     public void activate(boolean activation){
         SharedPreferences.Editor edit = prefs.edit();
         edit.putBoolean("activated",activation);
         edit.commit();
     }
 
+    /**
+     * set number of last received SMS
+     * @param number
+     */
     public void setNumber(String number){
         SharedPreferences.Editor edit = prefs.edit();
         edit.putString("Number",number);
         edit.commit();
     }
 
+    /**
+     * get number of last received sms
+     * @return
+     */
     public String getNumber(){
         return prefs.getString("Number","N/A");
     }
 
+    /**
+     * set video path
+     * @param path
+     */
     public void setVideoPath(String path){
         SharedPreferences.Editor edit = prefs.edit();
         edit.putString("videoPath",path);
         edit.commit();
     }
 
+
+    /**
+     * get video path
+     * @return
+     */
     public String getVideoPath(){
         return prefs.getString("videoPath","N/A");
     }
--- app/src/main/java/com/snapdrive/snapdrive/RecordService.java
+++ app/src/main/java/com/snapdrive/snapdrive/RecordService.java
@@ -19,39 +19,49 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.widget.Toast;
 public class RecordService extends Service{
+
+
     private static final String TAG = "RecordService";
     private SurfaceView mSurfaceView;
     private SurfaceHolder mSurfaceHolder;
     private static Camera mServiceCamera;
     private boolean mRecordingStatus;
     private MediaRecorder mMediaRecorder;
+
+
     @Override
     public void onCreate() {
         mRecordingStatus = false;
         mSurfaceView = CameraActivity.mSurfaceView;
         mSurfaceHolder = CameraActivity.mSurfaceHolder;
-// open front camera
+        // open front camera
         mServiceCamera = Camera.open(Camera.CameraInfo.CAMERA_FACING_FRONT);
         super.onCreate();
     }
+
+
     @Override
     public IBinder onBind(Intent intent) {
         return null;
     }
+
+
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         super.onStartCommand(intent, flags, startId);
-// if there are no records running, start one
+        // if there are no records running, start one
         if (mRecordingStatus == false)
             startRecording();
         return START_STICKY;
     }
+
     @Override
     public void onDestroy() {
         stopRecording();
         mRecordingStatus = false;
         super.onDestroy();
     }
+
     /**
      * start a recording
      */
@@ -59,7 +69,7 @@ public class RecordService extends Service{
         try {
             Toast.makeText(getBaseContext(), "Recording Started", Toast.LENGTH_SHORT).show();
             mServiceCamera.reconnect();
-// camera parameters
+            // camera parameters
             Camera.Parameters params = mServiceCamera.getParameters();
             mServiceCamera.setParameters(params);
             Camera.Parameters p = mServiceCamera.getParameters();
@@ -70,7 +80,7 @@ public class RecordService extends Service{
             p.setPreviewSize(mPreviewSize.width, mPreviewSize.height);
             p.setPreviewFormat(PixelFormat.YCbCr_420_SP);
             mServiceCamera.setParameters(p);
-// set the preview display, unfortunately we must have it
+            // set the preview display, unfortunately we must have it
             try {
                 mServiceCamera.setPreviewDisplay(mSurfaceHolder);
                 mServiceCamera.startPreview();
@@ -80,7 +90,7 @@ public class RecordService extends Service{
                 e.printStackTrace();
             }
             mServiceCamera.unlock();
-//MediaRecorder
+            //MediaRecorder
             mMediaRecorder = new MediaRecorder();
             mMediaRecorder.setCamera(mServiceCamera);
             mMediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
@@ -88,19 +98,22 @@ public class RecordService extends Service{
             mMediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
             mMediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);
             mMediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.DEFAULT);
-//mMediaRecorder.setOutputFile("/sdcard/video.mp4");
-// set the filePath in SnapDrive folder
-            mMediaRecorder.setOutputFile(getOutputMediaFile(2).getPath());
+
+            // set the filePath in SnapDrive folder
+            String path = getOutputMediaFile(2).getPath();
+            AppPreferences prefs = new AppPreferences(getApplicationContext());
+            prefs.setVideoPath(path);
+            mMediaRecorder.setOutputFile(path);
             mMediaRecorder.setVideoFrameRate(30);
             mMediaRecorder.setVideoSize(mPreviewSize.width, mPreviewSize.height);
-//mMediaRecorder.setVideoSize(320,400);
+
             mMediaRecorder.setPreviewDisplay(mSurfaceHolder.getSurface());
-// video duration
-            mMediaRecorder.setMaxDuration(6000);
+            // video duration
+            mMediaRecorder.setMaxDuration(5000);
             mMediaRecorder.prepare();
             mMediaRecorder.start();
-// listen the end of recording
-            mMediaRecorder.setOnInfoListener(new MediaRecorder.OnInfoListener() {
+            // listen the end of recording
+           mMediaRecorder.setOnInfoListener(new MediaRecorder.OnInfoListener() {
                 @Override
                 public void onInfo(MediaRecorder mr, int what, int extra) {
                     if (what == MediaRecorder.MEDIA_RECORDER_INFO_MAX_DURATION_REACHED) {
@@ -150,22 +163,24 @@ public class RecordService extends Service{
         i.putExtra("action","reponse");
         startService(i);
     }
+
+
     /** Create a File for saving an image or video */
     public static File getOutputMediaFile(int type){
-// To be safe, you should check that the SDCard is mounted
-// using Environment.getExternalStorageState() before doing this.
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
         File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
                 Environment.DIRECTORY_PICTURES), "SnapDrive");
-// This location works best if you want the created images to be shared
-// between applications and persist after your app has been uninstalled.
-// Create the storage directory if it does not exist
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+        // Create the storage directory if it does not exist
         if (! mediaStorageDir.exists()){
             if (! mediaStorageDir.mkdirs()){
                 Log.d("SnapDrive", "failed to create directory");
                 return null;
             }
         }
-// Create a media file name
+        // Create a media file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         File mediaFile;
         if (type == 1){
